eb455101bac1fddd4477804bd3ce24c33f7ee131
Rafal Stasiorowski <rstasiorowski@gmail.com>
2020-05-24 13:55:21 +0200

    rev.3

--- src/main/java/pizza_gui/controller/PizzaController.java
+++ src/main/java/pizza_gui/controller/PizzaController.java
@@ -1,5 +1,7 @@
 package pizza_gui.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
@@ -7,23 +9,31 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import pizza_gui.model.Ingredients;
+import pizza_gui.model.Pizza;
+import pizza_gui.model.PizzaModel;
+
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class PizzaController {
+    // Aby dodać kolekcję do obiektów do kontrolek FXML korzystamy ObservableLst
+    private ObservableList<PizzaModel> pizzas=FXCollections.observableArrayList();
 
     @FXML
-    private TableView<?> tblPizza;
+    private TableView<PizzaModel> tblPizza;   // Klasa modelu
 
     @FXML
-    private TableColumn<?, ?> tcName;
+    private TableColumn<PizzaModel, String> tcName;   //Klasa modelu, typ danych
 
     @FXML
-    private TableColumn<?, ?> tcIngridients;
+    private TableColumn<PizzaModel, String> tcIngridients;
 
     @FXML
-    private TableColumn<?, ?> tcType;
+    private TableColumn<PizzaModel, String> tcType;
 
     @FXML
-    private TableColumn<?, ?> tcPrice;
+    private TableColumn<PizzaModel, Double> tcPrice;
 
     @FXML
     private Label randomPizza;
@@ -44,6 +54,8 @@ public class PizzaController {
 
     @FXML
     void orderAction(MouseEvent event) {
+
+        System.out.println("Zamówiono");
 
     }
 
--- src/main/java/pizza_gui/service/PizzaSevice.java
+++ src/main/java/pizza_gui/service/PizzaSevice.java
@@ -1,4 +1,55 @@
 package pizza_gui.service;
 
+import javafx.collections.ObservableList;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import pizza_gui.model.Ingredients;
+import pizza_gui.model.Pizza;
+import pizza_gui.model.PizzaModel;
+
+
+import java.util.stream.Collectors;
+
 public class PizzaSevice {
+
+    //-----------------------------------------------------------------------------------------------------------------
+    //wprowadzić pizze do listy do listy pizzas-------------------
+    public ObservableList<PizzaModel> addPizzas(ObservableList<PizzaModel> pizzas){
+        for (Pizza pizza : Pizza.values()){
+            pizzas.add(new PizzaModel(
+                    pizza.getName(),
+                    pizza.getIngredients().stream().map(Ingredients::getName).collect(Collectors.joining(",")),
+                    (pizza.getIngredients().stream().anyMatch(Ingredients::isSpicy) ? "ostra " : " ")
+                            +
+                            (pizza.getIngredients().stream().noneMatch(Ingredients::isMeat) ? "wege " : " "),
+                    pizza.getIngredients().stream().mapToDouble(Ingredients::getPrice).sum()
+            ));
+        }
+        return pizzas;
+
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------
+        //metoda konfigurująca kolumny TableView i wprowadzająca dane z ObservableList -------------------
+
+    public void insertPizzasToTable(
+                    TableView<PizzaModel> tblPizza,
+            TableColumn<PizzaModel, String> tcName,
+            TableColumn<PizzaModel, String> tcIngredients,
+            TableColumn<PizzaModel, String> tcType,
+            TableColumn<PizzaModel, Double> tcPrice,
+            ObservableList<PizzaModel> pizzas
+    ){
+        tcName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        tcIngredients.setCellValueFactory(new PropertyValueFactory<>("ingredients"));
+        tcType.setCellValueFactory(new PropertyValueFactory<>("type"));
+        tcPrice.setCellValueFactory(new PropertyValueFactory<>("price"));
+        tblPizza.setItems(pizzas);
+
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------
+
+
 }
